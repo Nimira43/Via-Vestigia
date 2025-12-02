d2659b728bdaae453826da2af7b9b34396deb3ea
Nimira43 <n.rabone72@aol.com>
2025-12-02 08:05:03 +0000

    Created createUser with Post decorator

--- via-vestigia-nest/src/user/user.controller.ts
+++ via-vestigia-nest/src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common'
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  
+  @Post()
+  createUser() {
+    return 'Success'
+  }
+}
