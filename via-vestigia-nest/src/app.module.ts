6f693dc762f188e1a8e16fb173d07803c2a15c4e
Nimira43 <n.rabone72@aol.com>
2025-12-16 00:56:00 +0000

    Passed User into TypeOrmModule forFeature.

--- via-vestigia-nest/src/app.module.ts
+++ via-vestigia-nest/src/app.module.ts
@@ -4,12 +4,17 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './user/user.entity'
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: './app-db.sqlite',
-    synchronize: true,
-    entities: [User]
-  })],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './app-db.sqlite',
+      synchronize: true,
+      entities: [User]
+    }),
+    TypeOrmModule.forFeature([User])
+  
+  ],
+
   controllers: [UserController],
   providers: [],
 })
