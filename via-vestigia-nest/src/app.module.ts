6eaa80b710109216b2f4be4775c529fc4994a8a0
Nimira43 <n.rabone72@aol.com>
2025-12-16 00:50:13 +0000

    Added entities property and set it to the User class previously made.

--- via-vestigia-nest/src/app.module.ts
+++ via-vestigia-nest/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
 import { UserController } from './user/user.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './user/user.entity'
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
     database: './app-db.sqlite',
     synchronize: true,
+    entities: [User]
   })],
   controllers: [UserController],
   providers: [],
