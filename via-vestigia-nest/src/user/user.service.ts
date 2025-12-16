5a9652def7fa212a120dc95bf78b049f96706614
Nimira43 <n.rabone72@aol.com>
2025-12-16 02:14:01 +0000

    Passed the user object into createUser in UserService

--- via-vestigia-nest/src/user/user.service.ts
+++ via-vestigia-nest/src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { User } from './user.entity'
 import { Repository } from 'typeorm'
+import { CreateUser } from './dto/create-user.dto'
 
 @Injectable()
 export class UserService {
@@ -10,7 +11,14 @@ export class UserService {
     private userRepository: Repository<User>
   ) {}
   
-  async createUser(body: any): Promise<void> {
+  async createUser(
+    body: CreateUser
+  ): Promise<void> {
+    const user = {
+      name: body.email.split('@')[0],
+      handle: body.email.split('@')[0],
+      email: body.email,
+    }
     await this.userRepository.save(body)
   }
 }
