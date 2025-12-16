c5cada24b23a3c29c37c65e825734a281a6540b9
Nimira43 <n.rabone72@aol.com>
2025-12-16 00:47:42 +0000

    Added TypeOrmModule to imports in  app.module.ts. Starting configuration.

--- via-vestigia-nest/src/app.module.ts
+++ via-vestigia-nest/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common'
 import { UserController } from './user/user.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: './app-db.sqlite',
+    synchronize: true,
+  })],
   controllers: [UserController],
   providers: [],
 })
