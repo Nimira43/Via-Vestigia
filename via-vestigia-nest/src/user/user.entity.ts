a37d07f2aa4e56684d566d4c96a6c7a93a33f368
Nimira43 <n.rabone72@aol.com>
2025-12-16 00:41:46 +0000

    Added Column decorator to name, handle and email

--- via-vestigia-nest/src/user/user.entity.ts
+++ via-vestigia-nest/src/user/user.entity.ts
@@ -1,13 +1,16 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number
 
+  @Column()
   name: string
   
+  @Column()
   handle: string
-  
+
+  @Column()
   email: string
 }
