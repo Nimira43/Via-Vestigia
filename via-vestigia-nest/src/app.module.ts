322eb679f09f30f7da83cccee160151dddb49a32
Nimira43 <n.rabone72@aol.com>
2026-01-03 15:31:47 +0000

    Created UserModule in Nest..

--- via-vestigia-nest/src/app.module.ts
+++ via-vestigia-nest/src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common'
-import { UserController } from './user/user.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './user/user.entity'
-import { UserService } from './user/user.service'
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -11,12 +10,11 @@ import { UserService } from './user/user.service'
       database: './app-db.sqlite',
       synchronize: true,
       entities: [User]
-    }),
-    TypeOrmModule.forFeature([User])
-  
+    }),   
+    UserModule,
   ],
 
-  controllers: [UserController],
-  providers: [UserService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
