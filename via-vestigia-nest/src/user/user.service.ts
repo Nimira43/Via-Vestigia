370cb3b753e07eede741e29e1233e227048dc788
Nimira43 <n.rabone72@aol.com>
2026-04-05 01:40:01 +0100

    Mapping DB constraint errors.

--- via-vestigia-nest/src/user/user.service.ts
+++ via-vestigia-nest/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { User } from './user.entity'
 import { Repository } from 'typeorm'
@@ -19,17 +19,35 @@ export class UserService {
     user.name = body.email.split('@')[0]
     user.handle = user.name
 
-    const userInDB = this.userRepository.findOneBy({ handle: user.handle})
+    const userInDB = this.userRepository.findOneBy({ 
+      handle: user.handle
+    })
 
     if (userInDB) {
       user.handle = user.name + generateUniqueValue(true)
     }
 
     user.registrationToken = generateUniqueValue()
-    await this.userRepository.save(user)
-    await this.emailService.sendSignUpEmail(
-      user.email,
-      user.registrationToken
-    )
+    try {
+      await this.userRepository.save(user)
+      await this.emailService.sendSignUpEmail(
+        user.email,
+        user.registrationToken
+      )
+    } catch (exception) {
+      // if (exception.message.includes('UNIQUE constraint')) {
+      if (exception instanceof Error && exception.message.includes('UNIQUE constraint')) {
+        throw new BadRequestException(
+          'Invalid request', { 
+            cause: [
+              { 
+                property: 'email',
+                constraints: ['Email is in use']
+              }
+            ]
+          }
+        )
+      }
+    }
   }
 }
